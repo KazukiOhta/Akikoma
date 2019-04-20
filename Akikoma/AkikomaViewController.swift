372cd7f9470d1ae57497103f13cb9387ec5db89a
KazukiOhta <kazukiota121@gmail.com>
2019-04-20 14:26:38 +0900

    とりあえずCollectionViewで表が作れた。

---
+++ Akikoma/AkikomaViewController.swift
@@ -0,0 +1,70 @@
+//
+//  AkikomaViewController.swift
+//  Akikoma
+//
+//  Created by 太田 一毅 on 2019/04/20.
+//  Copyright © 2019 太田 一毅. All rights reserved.
+//
+
+import UIKit
+
+class AkikomaViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if section == 0{
+            return 30 //
+        } else {
+            return 9
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let section: Int = indexPath.section
+        let cell: UICollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        if section == 0{
+            // Akikoma Cells
+            let label = cell.contentView.viewWithTag(1) as! UILabel
+            label.backgroundColor = UIColor.white
+            label.numberOfLines = 1
+            label.textAlignment = NSTextAlignment.center
+            label.font = UIFont(name: "Futura", size: 20)
+            label.text = String(indexPath.row)
+        } else {
+            // People Cells
+            let label = cell.contentView.viewWithTag(1) as! UILabel
+            label.backgroundColor = UIColor.white
+            label.numberOfLines = 2
+            label.textAlignment = NSTextAlignment.left
+            label.font = UIFont(name: "Futura", size: 10)
+            label.text = "名前：　" + String(indexPath.row) + "\n" + "あきこまコード：　" + String(17*indexPath.row)//!!
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if indexPath.section == 0{
+            let horizontalMargin: CGFloat = 2 //横方向のMargin
+            let cellSize: CGFloat = self.view.bounds.width/5 - horizontalMargin
+            return CGSize(width: cellSize, height: cellSize)
+        } else {
+            let horizontalMargin: CGFloat = 2 //横方向のMargin
+            let cellSize: CGFloat = self.view.bounds.width/3 - horizontalMargin
+            return CGSize(width: cellSize, height: cellSize/2)
+        }
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 2
+    }
+
+}
