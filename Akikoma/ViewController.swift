313569363bffac975407538c1734cbec9ce97368
KazukiOhta <kazukiota121@gmail.com>
2019-04-20 12:13:46 +0900

    Initial Commit

---
+++ Akikoma/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Akikoma
+//
+//  Created by 太田 一毅 on 2019/04/20.
+//  Copyright © 2019 太田 一毅. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
